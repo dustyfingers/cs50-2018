6a4d78458a597ab8d6515026207b81dc50334550
dustyfingers <dustyfingers01257@gmail.com>
2019-05-28 22:29:25 -0600

    initial commit

---
+++ lec-2/helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int counter = 0;
+    counter += 1;
+    printf("Hello World\n");
+    if (5 > 3) 
+    {
+        printf("5 is greater than 3\n");
+    }
+    else
+    {
+        printf("5 is not greater than 3\n");
+    }
+}
